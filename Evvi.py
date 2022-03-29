120a7c0118ed6f49987edf8d4887a20f07c6e48d
RaaVanansri <vigneshseetharam@outlook.com>
2022-03-29 09:56:46 +0530

    auto bump set

--- Evvi.py
+++ Evvi.py
@@ -2,13 +2,30 @@ import os
 import discord
 import requests
 import json
+import pymongo
 from dotenv import load_dotenv
 load_dotenv()
 
 #client = commands.Bot(command_prefix="/", case_insensitive=True)
 
 client = discord.Client()
-my_secret = os.getenv('TOKEN')
+evvi = os.getenv('TOKEN')
+ella = os.getenv('FILTH')
+el = os.getenv('RAAVANAN')
+db = pymongo.MongoClient("mongodb://raavanan:Sr1kutty77@cluster0-shard-00-00.z89ji.mongodb.net:27017,cluster0-shard-00-01.z89ji.mongodb.net:27017,cluster0-shard-00-02.z89ji.mongodb.net:27017/Channel?ssl=true&replicaSet=atlas-5yhivq-shard-0&authSource=admin&retryWrites=true&w=majority")
+
+vardb = db["Channel"]
+c_id = vardb["channel_id"]
+bcid = {"bumpcid":0}
+
+x = c_id.insert_one(bcid)
+
+print(bcid)
+
+def bumpchannel(cid):
+  bcid_update = {"bumpcid":cid}
+  x = c_id.update_one(bcid,bcid_update)
+
 
 def get_quote():
   response = requests.get('https://zenquotes.io/api/random')
@@ -41,18 +58,24 @@ async def on_message(message):
             #print("Something")
             #or
       await message.channel.send(f"Aabasam thaveerpom friends,  <@{message.author.id}>")
+      await message.delete()
       await client.process_commands(message)
 
   if message.content.startswith('/filth'):
-    if message.author.id == 861169200968892417:
-      await message.channel.send(f'Hey <@{929079385737281586}>, <@{861169200968892417}> asking for hugs')
+    if message.author.id == el:
+      await message.channel.send(f'Hey <@{ella}>, <@{el}> asking for hugs')
     else:
       await message.channel.send('Sorry fella, only raavanan can use this command')
 
   if message.content.startswith('/raavanan'):
-    if message.author.id == 929079385737281586:
-      await message.channel.send(f'Hey <@{861169200968892417}>, your gorl <@{929079385737281586}> asking for hugs you slug')
+    if message.author.id == ella:
+      await message.channel.send(f'Hey <@{el}>, your gorl <@{ella}> asking for hugs you slug')
     else:
       await message.channel.send('Sorry fella, only filth can use this command')
 
-client.run(my_secret)
+  if message.content.startswith('/set auto bump'): 
+    b1cid = {"bumpcid": message.channel.id}
+    x = c_id.update_one({'_id': '624288e2c9fced7e94938ae4'},{"$set": {"bumpcid":message.channel.id}}, upsert=True)
+
+
+client.run(evvi)
